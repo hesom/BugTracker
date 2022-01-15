354f66e52428302f5e736f97038403fa1e062ef7
hesom <sommerhoff.hendrik@gmail.com>
2022-01-15 21:29:17 +0100

    Add form validation

--- client/src/components/AddBugForm.js
+++ client/src/components/AddBugForm.js
@@ -9,14 +9,31 @@ const AddBugForm = ({ addBug }) => {
     const [reporter, setReporter] = useState("")
     const [date, setDate] = useState("")
     const [status, setStatus] = useState("open")
-    const [assigned, setAssigned] = useState("")
+    const [assigned, setAssigned] = useState("unassigned")
     const [severity, setSeverity] = useState("low")
     const [description, setDescription] = useState("")
+    const [errors, setErrors] = useState({
+        title: "",
+        reporter: "",
+        date: "",
+        assigned: "",
+    })
 
     const navigate = useNavigate()
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        
+        setErrors({
+            title: title === '' ? "error" : '',
+            reporter: reporter === '' ? "error" : '',
+            date: date === '' ? "error" : '',
+            assigned: assigned === '' ? "error" : '',
+        })
+
+        if(title === '' || reporter === '' || date === '' || assigned === ''){
+            return;
+        }
 
         const newBug = {
             short: title,
@@ -38,11 +55,11 @@ const AddBugForm = ({ addBug }) => {
             <div className="bug-form-container">
                 <div className="bug-form-input">
                     <label htmlFor="title">Title: </label>
-                    <input type="text" name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <input type="text" name='title' id='title' value={title} onChange={(e) => setTitle(e.target.value)} className={errors.title}/>
                     <label htmlFor="reporter">Reporter: </label>
-                    <input type="text" name='reporter' id='reporter' value={reporter} onChange={(e) => setReporter(e.target.value)}/>
+                    <input type="text" name='reporter' id='reporter' value={reporter} onChange={(e) => setReporter(e.target.value)} className={errors.reporter}/>
                     <label htmlFor="date">Date: </label>
-                    <input type="date" name="date" id='date' value={date} onChange={(e) => setDate(e.target.value)}/>
+                    <input type="date" name="date" id='date' value={date} onChange={(e) => setDate(e.target.value)} className={errors.date}/>
                     <label htmlFor="status">Status: </label>
                     <select name="status" id="status" value={status} onChange={(e) => setStatus(e.target.value)}>
                         <option value="open">Open</option>
@@ -50,7 +67,7 @@ const AddBugForm = ({ addBug }) => {
                         <option value="closed">Closed</option>
                     </select>
                     <label htmlFor="assigned-to">Assigned To: </label>
-                    <input type="text" name="assigned-to" id="assigned-to" value={assigned} onChange={(e) => setAssigned(e.target.value)}/>
+                    <input type="text" name="assigned-to" id="assigned-to" value={assigned} onChange={(e) => setAssigned(e.target.value)} className={errors.assigned}/>
                     <label htmlFor="severity">Severity</label>
                     <select name="severity" id="severity" value={severity} onChange={(e) => setSeverity(e.target.value)}>
                         <option value="low">Low</option>
